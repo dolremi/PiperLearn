568ea0d96fce7915f25df68fbf79e80e0d64befc
Jia Xu <dolremi@gmail.com>
2017-11-01 17:15:48 -0500

    Modify the plot single column function

--- piperlearn/visual/statsplot.py
+++ piperlearn/visual/statsplot.py
@@ -19,7 +19,48 @@ class correlations(object):
         plot_single(self.corr, target, figsize, sorted=True, abs=True, horizontal=True)
 
 
-def plot_single(self, data, target, figsize, column=None, sorted=False, abs=False, horizontal=True):
+def plot_single(data, target, figsize, column=None, original=True, percentage=False, sorted=False, abs=False,
+                horizontal=True, precision=2, **kwargs):
+    col = transform_display(abs, column, data, sorted, target, original)
+    total = 0
+
+    if horizontal:
+        ax = col.plot.barh(figsize=figsize)
+        if percentage:
+            totals = []
+            for i in ax.patches:
+                totals.append(i.get_width())
+            total = sum(totals)
+
+        for p in ax.patches:
+            if percentage:
+                display = str(round((p.get_width()/total) * 100), precision) + '%'
+            else:
+                display = str(round((p.get_width(), precision)))
+            if p.get_width() > 0:
+                ax.text(p.get_width() + 0.01, p.get_y() + 0.15, display, **kwargs)
+            else:
+                ax.text(p.get_width() - 0.01, p.get_y() + 0.15, display, **kwargs)
+    else:
+        ax = col.plot.bar(figsize=figsize)
+        if percentage:
+            totals = []
+            for i in ax.patches:
+                totals.append(i.get_height())
+            total = sum(totals)
+
+        for p in ax.patches:
+            if percentage:
+                display = str(round((p.get_height()/total) * 100), precision) + '%'
+            else:
+                display = str(round((p.get_height(), precision)))
+            if p.get_height() > 0:
+                ax.text(p.get_x() - 0.01, p.get_height() + 0.15, display, **kwargs)
+            else:
+                ax.text(p.get_x() - 0.01, p.get_height() + 0.15, display, **kwargs)
+
+
+def transform_display(abs, column, data, sorted, target, original):
     dataset = check_dataframe(data, column)
     check_cols(dataset, target)
     if sorted:
@@ -29,20 +70,9 @@ def plot_single(self, data, target, figsize, column=None, sorted=False, abs=Fals
             dataset['sorted'] = dataset[target]
         dataset.sort(columns='sorted', inplace=True)
         dataset.drop('sorted', axis=1)
-    if horizontal:
-        ax = dataset[target].plot.barh(figsize=figsize)
-        for p in ax.patches:
-            if p.get_width() > 0:
-                ax.text(p.get_width() + 0.01, p.get_y() + 0.15, str(round((p.get_width()), 4)), color='dimgrey')
-            else:
-                ax.text(p.get_width() - 0.01, p.get_y() + 0.15, str(round((p.get_width()), 4)), color='dimgrey')
-    else:
-        ax = dataset[target].plot.bar(figsize=figsize)
-        for p in ax.patches:
-            if p.get_height() > 0:
-                ax.text(p.get_x() - 0.01, p.get_height() + 0.15, str(round((p.get_height()), 4)), color='dimgrey')
-            else:
-                ax.text(p.get_x() - 0.01, p.get_height() + 0.15, str(round((p.get_height()), 4)), color='dimgrey')
+    if original:
+        return dataset[target]
+    return dataset[target].value_counts()
 
 
 
